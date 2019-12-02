281e2e49d366f6249dfd3d0acbee4c367e73a80a
Chayim Refael Friedman <chayimfr@gmail.com>
2019-12-02 11:54:17 +0200

    Add typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,15 @@
+/// <reference types="chai" />
 
+declare global {
+  namespace Chai {
+    interface Members {
+      with: ChaiIncludeOrderedMembersWithGaps.MembersWith;
+    }
+  }
+}
+
+declare namespace ChaiIncludeOrderedMembersWithGaps {
+  interface MembersWith {
+    gaps(set: readonly any[], message?: string): void;
+  }
+}
