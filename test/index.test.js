c7499bfa69731e13d09539912b9c48355a62533f
חיים רפאל פרידמן <chayimre@acad.jct.ac.il>
2019-12-02 17:13:44 +0200

    Test for deep comparison

--- test/index.test.js
+++ test/index.test.js
@@ -30,5 +30,9 @@ describe('members', () => {
     it('should not match if elements are not ordered', () => {
       expect([2, 1, 3]).to.not.include.ordered.members.with.gaps([1, 2, 3]);
     });
+
+    it("should honor 'deep'", () => {
+      expect([{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]).to.include.deep.ordered.members.with.gaps([{ a: 1 }, { c: 3 }]);
+    });
   });
 });
