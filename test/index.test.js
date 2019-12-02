03aeb9fa223a5e0058f93fab40e7a5bd5b659478
Chayim Refael Friedman <chayimfr@gmail.com>
2019-12-02 12:27:42 +0200

    Add test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,13 @@
 'use strict';
 
-const { expect, should, assert } = require('chai');
+const { expect, should, assert, use } = require('chai');
 
 should();
-chai.use(require('..'));
+use(require('..'));
+
+describe('members', () => {
+  it('should use the original method when used as a method', () => {
+    expect([1, 2, 3]).to.include.ordered.members([1, 2, 3]);
+    expect([0, 1, 2, 3]).to.not.include.ordered.members([1, 2, 3]);
+  });
+});
