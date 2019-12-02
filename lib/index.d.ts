aa35510e27b46ea02ca4741b329624b8f0fc7168
חיים רפאל פרידמן <chayimre@acad.jct.ac.il>
2019-12-02 17:19:49 +0200

    Add TDD typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,6 +5,13 @@ declare global {
     interface Members {
       with: ChaiIncludeOrderedMembersWithGaps.MembersWith;
     }
+
+    interface Assert {
+      includeOrderedMembersWithGaps<T>(superset: T[], subset: T[]): void;
+      includeDeepOrderedMembersWithGaps<T>(superset: T[], subset: T[]): void;
+      notIncludeOrderedMembersWithGaps<T>(superset: T[], subset: T[]): void;
+      notIncludeDeepOrderedMembersWithGaps<T>(superset: T[], subset: T[]): void;
+    }
   }
 }
 
